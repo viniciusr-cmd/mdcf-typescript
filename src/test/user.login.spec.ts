50576aecaa0b2dcae2ad51977c0f0999767cc096
viniciusr-cmd <vrs122@gmail.com>
2024-02-24 15:37:47 -0300

    Fix: import and remove unused sessiontoken variable

--- src/test/user.login.spec.ts
+++ src/test/user.login.spec.ts
@@ -2,10 +2,10 @@ import { test, expect, describe, beforeAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../index'
 import { UserModel } from '../db/users';
+import { get } from 'lodash';
 
 // Create testuser from the database before running the spec if it does not exist
 const username = 'testuser'
-let sessiontoken = ''
 
 beforeAll(async () => {
     await UserModel.findOneAndDelete({ username });
@@ -15,18 +15,12 @@ beforeAll(async () => {
             username: username,
             password: 'testpassword'
         })
-        // Get session token in body
-        .then((res) => {
-            sessiontoken = res.body.authentication.sessionToken
-        })
-    console.log(sessiontoken)
 });
 
 describe('User Login', () => {
     test('POST /auth/login - should login a user and return 200 status', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
-            .set('Cookie', `sessionToken=${sessiontoken}`)
             .send({
                 username: username,
                 password: 'testpassword'
@@ -35,25 +29,22 @@ describe('User Login', () => {
         expect(res.body).toHaveProperty('authentication.sessionToken')
     })
 
-    test('POST /auth/login - should return 401 status for incorrect credentials', async ({ expect }) => {
+    test('POST /auth/login - should return 400 status for incorrect credentials', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
-            .set('Cookie', `sessionToken=${sessiontoken}`)
             .send({
                 username: 'wronguser',
                 password: 'wrongpassword'
             })
-            console.log(res.text)
-        expect(res.statusCode).toBe(401)
+        expect(res.statusCode).toBe(400)
     })
 
-    test('POST /auth/login - should return 403 for token that is invalid', async ({ expect }) => {
+    test('POST /auth/login - should return 403 for a distinct password in database that is invalid', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
-            .set('Cookie', `${sessiontoken}1234`)
             .send({
                 username: 'testuser',
-                password: 'testpassword'
+                password: 'invalidpassword'
             })
         expect(res.statusCode).toBe(403)
     })
