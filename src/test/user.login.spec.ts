e488dc784260503840668cf1ca43f620772449e5
viniciusr-cmd <vrs122@gmail.com>
2024-02-24 14:58:40 -0300

    Fix: Add user session token to login

--- src/test/user.login.spec.ts
+++ src/test/user.login.spec.ts
@@ -1,22 +1,32 @@
 import { test, expect, describe, beforeAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../index'
+import { UserModel } from '../db/users';
 
 // Create testuser from the database before running the spec if it does not exist
 const username = 'testuser'
+let sessiontoken = ''
+
 beforeAll(async () => {
+    await UserModel.findOneAndDelete({ username });
     await request(app)
-            .post('/auth/register')
-            .send({
-                username: username,
-                password: 'testpassword'
-            })
+        .post('/auth/register')
+        .send({
+            username: username,
+            password: 'testpassword'
+        })
+        // Get session token in body
+        .then((res) => {
+            sessiontoken = res.body.authentication.sessionToken
+        })
+    console.log(sessiontoken)
 });
 
 describe('User Login', () => {
     test('POST /auth/login - should login a user and return 200 status', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
+            .set('Cookie', `sessionToken=${sessiontoken}`)
             .send({
                 username: username,
                 password: 'testpassword'
@@ -28,17 +38,19 @@ describe('User Login', () => {
     test('POST /auth/login - should return 401 status for incorrect credentials', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
+            .set('Cookie', `sessionToken=${sessiontoken}`)
             .send({
                 username: 'wronguser',
                 password: 'wrongpassword'
             })
+            console.log(res.text)
         expect(res.statusCode).toBe(401)
     })
 
     test('POST /auth/login - should return 403 for token that is invalid', async ({ expect }) => {
         const res = await request(app)
             .post('/auth/login')
-            .set('Cookie', 'sessionToken=invalidToken')
+            .set('Cookie', `${sessiontoken}1234`)
             .send({
                 username: 'testuser',
                 password: 'testpassword'
